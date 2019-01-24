c53f9d150e8c3b7665505b12a49873d2f7ec4614
Andrew Morgan <andrew@amorgan.xyz>
2019-01-24 22:57:54 +0000

    Use tslint

--- src/mithril-zeroframe-router.ts
+++ src/mithril-zeroframe-router.ts
@@ -6,38 +6,41 @@
 // in order to instruct mithril on which hash to load initially from the outer
 // page URL.
 
-import {RouteResolver, Comp} from 'mithril'
+import {Comp, RouteResolver} from "mithril";
 
-import ZeroFrame from 'zeroframe'
-const zeroframe = new ZeroFrame()
+import ZeroFrame from "zeroframe";
+const zeroframe = new ZeroFrame();
 
-// Resolver is a mithril RouteResolver <https://mithril.js.org/route.html#routeresolver>
-// that acts as a thin wrapper around a mithril Component. Instead of simply
-// handing a Component object `MyComponent` to the mithril router, you instead
-// give it `Resolver(MyComponent)`. When the route is being changed within the
-// iframe, such as navigating to `/#!/hello`, by default this will not be
-// reflected outside of the iframe (aka the user's address bar). Resolver, on
-// being notified the route is being changed, will make a call to ZeroFrame,
-// telling it to update the page URL to match the new route.
+// Resolver is a function that returns a mithril RouteResolver
+// <https://mithril.js.org/route.html#routeresolver>, and acts as a thin wrapper
+// around a mithril Component. Instead of simply handing a Component object, say
+// `MyComponent` to the mithril router, you instead give it
+// `Resolver(MyComponent)`.
+//
+// When the route is being changed within the iframe,
+// such as navigating to `/#!/hello`, by default this will not be reflected
+// outside of the iframe (aka the user's address bar). resolver, on being
+// notified the route is being changed, will make a call to ZeroFrame, telling
+// it to update the page URL to match the new route.
 //
 // It then just returns the Component that was handed to it so all original
 // functionality is preserved.
-let Resolver = function(component: Comp<{}, {}>) {
-    let resolver: RouteResolver<{}, {}> = {
+const Resolver = (component: Comp<{}, {}>) => {
+    const resolver: RouteResolver<{}, {}> = {
         onmatch(attrs, path) {
             // Retrieve the current details of the inner iframe page
-            let yOffset = window.pageYOffset
-            let urlPath = window.location.hash
-            let title = document.title
+            const yOffset = window.pageYOffset;
+            const urlPath = window.location.hash;
+            const title = document.title;
 
             // Update the outer page to match
-            zeroframe.cmd('wrapperPushState', [{pageYOffset: yOffset}, title, urlPath])
+            zeroframe.cmd("wrapperPushState", [{pageYOffset: yOffset}, title, urlPath]);
 
             // Return the original component to the router
-            return component
-        }
-    }
-    return resolver
-}
+            return component;
+        },
+    };
+    return resolver;
+};
 
-export default Resolver
+export default Resolver;
